c6b7dc4d286102d444449e55288a8546ac26af19
Ricky Su <ricky@ez2.us>
2013-02-05 10:52:22 +0800

    add isset unset op

---
+++ lib/PHPPHP/LLVMEngine/OpLines/IssetOp.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace PHPPHP\LLVMEngine\OpLines;
+
+use PHPPHP\LLVMEngine\Zval as LLVMZval;
+use PHPPHP\LLVMEngine\Type\Base as BaseType;
+use PHPPHP\LLVMEngine\Internal\Module as InternalModule;
+
+class IssetOp extends OpLine {
+
+    use Parts\TypeCast,
+        Parts\PrepareOpZval;
+
+    protected $tmpZval = array();
+
+    public function write() {
+        parent::write();
+        $resultZval = $this->prepareResultZval();
+        $op1Var = $this->opCode->op1->getImmediateZval();
+
+        if (!$this->function->isZvalIRDefined($op1Var->getName())) {
+            $this->writeImmediateValueAssign($resultZval, false);
+            return;
+        }
+        $op1Zval=$this->function->getZvalIR($op1Var->getName(),false);
+        $this->testNULL($resultZval, $op1Zval);
+    }
+
+    protected function testNULL(LLVMZval $resultZval, LLVMZval $op1Zval) {
+        $op1ZvalPtr = $op1Zval->getPtrRegister();
+        $isNULL = $this->function->getRegisterSerial();
+        $this->function->writeOpLineIR("$isNULL = icmp eq ".LLVMZval::zval('*')." {$op1Zval->getPtrRegister()}, null");
+        $isNotNULLLabel=substr($this->function->getRegisterSerial(),1)."_is_NOT_NULL";
+        $isNULLLabel=substr($this->function->getRegisterSerial(),1)."_isNULL";
+        $isNULLEndifLabel=substr($this->function->getRegisterSerial(),1)."_Endif";
+        $this->function->writeOpLineIR("br i1 $isNULL, label  %$isNULLLabel, label %$isNotNULLLabel");
+        $this->function->writeOpLineIR("$isNULLLabel:");
+        $this->writeImmediateValueAssign($resultZval, false);
+        $this->function->writeOpLineIR("br label %$isNULLEndifLabel");
+        $this->function->writeOpLineIR("$isNotNULLLabel:");
+        $this->writeImmediateValueAssign($resultZval, true);
+        $this->function->writeOpLineIR("br label %$isNULLEndifLabel");
+        $this->function->writeOpLineIR("$isNULLEndifLabel:");
+    }
+
+}
---
+++ lib/PHPPHP/LLVMEngine/OpLines/UnsetOp.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace PHPPHP\LLVMEngine\OpLines;
+
+use PHPPHP\LLVMEngine\Zval as LLVMZval;
+use PHPPHP\LLVMEngine\Type\Base as BaseType;
+use PHPPHP\LLVMEngine\Internal\Module as InternalModule;
+
+class UnsetOp extends OpLine {
+
+    use Parts\TypeCast,
+        Parts\PrepareOpZval;
+
+    protected $tmpZval = array();
+
+    public function write() {
+        parent::write();
+        $resultZval = $this->prepareResultZval();
+        $op1Var = $this->opCode->op1->getImmediateZval();
+
+        if (!$this->function->isZvalIRDefined($op1Var->getName())) {
+            return;
+        }
+        $op1Zval=$this->function->getZvalIR($op1Var->getName(),false);
+        $this->gcVarZval($op1Zval);
+    }
+
+}
